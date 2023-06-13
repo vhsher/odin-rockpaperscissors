170a4014b8fb99010c3d8d5bdfc4181ab317acef
vhsher <76752087+vhsher@users.noreply.github.com>
2023-06-14 02:01:27 +1200

    Delete all game logic expect computerChoice

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+rock.addEventListener('click', playRound);
+paper.addEventListener('click', playRound);
+scissors.addEventListener('click', playRound);
+
 function getComputerChoice() {
   // Generate a random number from 0 to 2
   let randomNum = Math.floor(Math.random() * 3); 
@@ -5,13 +13,13 @@ function getComputerChoice() {
 
   switch (randomNum) {
     case 0:
-      result = "Rock";
+      result = "rock";
       break;
     case 1:
-      result = "Paper";
+      result = "paper";
       break;
     case 2:
-      result = "Scissors";
+      result = "scissors";
       break;
   }
 
@@ -19,95 +27,12 @@ function getComputerChoice() {
 
 }
 
-function capitalizeFirstLetter(string) { // Capitalize 1st letter for beauty
-  return string.charAt(0).toUpperCase() + string.slice(1);
-}
-
-function playRound(playerSelection, computerSelection) {
-  
-  computerSelection = getComputerChoice();
-  playerSelection = capitalizeFirstLetter( // Player turn
-    prompt("Your turn (Rock / Paper / Scissors)")
-  );
-
-  // Check playerSelection == Rock / Paper / Scissors
-  while ( (playerSelection != "Rock") &&
-          (playerSelection != "Paper") &&
-          (playerSelection != "Scissors") ) {
-    playerSelection = capitalizeFirstLetter(
-      prompt("Please write Rock / Paper / Scissors")
-      );
-  }
-
-  // Check who will win: Computer vs Player
-  if (computerSelection === "Rock") {
-
-    if (playerSelection === "Paper") {
-      return "You Win! Paper beats Rock";
-    } else if (playerSelection === "Scissors") {
-      return "You Lose! Rock beats Scissors";
-    } else { // Player = Rock
-      return "Draw! Two Rocks!";
-    }
-
-  } else if (computerSelection === "Paper") {
-
-    if (playerSelection === "Rock") {
-      return "You Lose! Paper beats Rock";
-    } else if (playerSelection === "Scissors") {
-      return "You Win! Scissors beats Paper";
-    } else { // Player = Paper
-      return "Draw! Two Papers!";
-    }
-
-  } else { // Computer = Scissors
-
-    if (playerSelection === "Rock") {
-      return "You Win! Rock beats Scissors";
-    } else if (playerSelection === "Paper") {
-      return "You Lose! Scissors beats Paper";
-    } else { // Player = Scissors
-      return "Draw! Two Scissors!";
-    }
-
-  }
+function playRound(e) {
+  let playerSelection = e.target.id;
+  let computerSelection = getComputerChoice();
 
 }
 
 function game() {
-  let drawCount = 0
-  let winCount = 0
-  let loseCount = 0
-  let gameCount = 0
-  
-  // Play 5 rounds
-  while (gameCount < 5) {
-    let game = playRound();
-    alert("ROUND RESULTS: " + game);
-    console.log("ROUND RESULTS: " + game);
-
-    if (game.includes("Win")) {
-      winCount += 1;
-    } else if (game.includes("Lose")) {
-      loseCount += 1;
-    } else if (game.includes("Draw")) {
-      drawCount += 1;
-    } else {
-      undefined;
-    }
-
-    gameCount++;
-  }
-
-  // Game Results
-  if (winCount > loseCount) {
-    alert("You win the game! Congratulations!");
-    return "You win the game! Congratulations!";
-  } else if (winCount === loseCount) {
-    alert("Drawn game!");
-    return "Drawn game!";
-  } else { // Win < Lose
-    alert("You Lose. Good luck next time!");
-    return "You Lose. Good luck next time!";
-  }
+ 
 }

bff4fa6de5f13d0277bd8f82d4c29cab17b73797
vhsher <76752087+vhsher@users.noreply.github.com>
2023-06-14 21:17:08 +1200

    Rework playRound function

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const rock = document.querySelector("#rock");
 const paper = document.querySelector("#paper");
 const scissors = document.querySelector("#scissors");
+const roundResults = document.querySelector("#round-results")
 
 rock.addEventListener('click', playRound);
 paper.addEventListener('click', playRound);
 scissors.addEventListener('click', playRound);
+
+let playerScore = 0
+let computerScore = 0
+
+if (playerScore === 5 || computerScore === 5) {
+  // Someone Wins!
+}
 
 function getComputerChoice() {
   // Generate a random number from 0 to 2
@@ -31,8 +39,43 @@ function playRound(e) {
   let playerSelection = e.target.id;
   let computerSelection = getComputerChoice();
 
-}
+  switch (playerSelection) {
 
-function game() {
- 
+    case "rock":
+      if (computerSelection === "paper") {
+        computerScore++;
+        roundResults.textContent = "Computer wins. Paper beats Rock";
+      } else if (computerSelection === "scissors") {
+        playerScore++;
+        roundResults.textContent = "You win! Rock beats Scissors";
+      } else { // computerSelection === "rock"
+        roundResults.textContent = "Draw, Rock vs Rock";
+      }
+      break;
+    
+    case "paper":
+      if (computerSelection === "rock") {
+        playerScore++;
+        roundResults.textContent = "You win! Paper beats rock";
+      } else if (computerSelection === "scissors") {
+        computerScore++;
+        roundResults.textContent = "Computer wins. Scissors beats Paper";
+      } else { // computerSelection === "paper"
+        roundResults.textContent = "Draw, Paper vs Paper";
+      }
+      break;
+  
+    case "scissors":
+      if (computerSelection === "rock") {
+        computerScore++;
+        roundResults.textContent = "Computer wins. Rock beats Scissors";
+      } else if (computerSelection === "paper") {
+        playerScore++;
+        roundResults.textContent = "You win! Scissors beats Paper";
+      } else { // computerSelection === "scissors"
+        roundResults.textContent = "Draw, Scissors vs Scissors";
+      }
+      break;
+
+  }
 }
